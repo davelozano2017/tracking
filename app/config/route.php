ed6cc6aaa39e29c7673276f72624abb5f914ca01
davelozano2017 <lozanojohndavid@gmail.com>
2019-08-17 12:40:15 +0800

    Create Login Page

--- app/config/route.php
+++ app/config/route.php
@@ -30,7 +30,7 @@ $routes = array();
     Example welcome change to anything you like
     just make sure the controller is existing
 */
-$routes['default_controller'] = 'Welcome';
+$routes['default_controller'] = 'Login';
 $routes['default_method'] = 'index';
 $routes['default_errors'] = 'errors';
 

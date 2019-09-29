646a2f7aa6e5e03b671e169b23647ecfdf74580a
davelozano2017 <lozanojohndavid@gmail.com>
2019-09-29 15:29:43 +0800

    View Driver By Id ( Front End )

--- app/views/pages/courier/drivers/view.php
+++ app/views/pages/courier/drivers/view.php
@@ -1,12 +1,12 @@
 <?php extract($data);?>
-<?php foreach($transactions as $key => $value) {} ?>
+
 <div class="content-wrapper">
 
 <!-- Page header -->
 <div class="page-header page-header-light">
   <div class="page-header-content header-elements-md-inline">
     <div class="page-title d-flex">
-      <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold"><?=$value[0]['ConsigneeName']?></span></h4>
+      <h4><i class="icon-arrow-left52 mr-2"></i> <span class="font-weight-semibold">John Doe <?=$id?></span></h4>
     </div>
   </div>
 
@@ -14,9 +14,9 @@
     <div class="d-flex">
       <div class="breadcrumb">
         <a href="#" class="breadcrumb-item">Dashboard</a>
-        <a href="#" class="breadcrumb-item">Transactions</a>
+        <a href="#" class="breadcrumb-item">Drivers</a>
         <a href="#" class="breadcrumb-item">View</a>
-        <a href="#" class="breadcrumb-item active"><?=$value[0]['ConsigneeName']?></a>
+        <a href="#" class="breadcrumb-item active">John Doe <?=$id?></a>
       </div>
 
     </div>
@@ -33,124 +33,55 @@
   <!-- Sidebars overview -->
   <div class="card">
     <div class="card-body">
-      <form action="<?=site_url('Transactions/UpdateTransactions')?>" method="POST" data-parsley-validate>
-        <input type="hidden" class="form-control" name="transactions_id" value="<?=encode($value[0]['transactions_id'])?>" required>
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">AWB #:</label>
-          <div class="col-lg-10">
-          <input type="number" class="form-control" name="awb_number" value="<?=$value[0]['awbNumber']?>" required>
+      <form action="<?=site_url('Accounts/UpdateUserById')?>" method="POST" data-parsley-validate>
+      <input type="hidden" name="accounts_id" value="<?=encode($getUsers[0]['accounts_id'])?>">
+      <input type="hidden" name="token" value="<?=$_SESSION['token']?>">
+          <div class="form-group row">
+            <label class="col-form-label col-lg-2">Name:</label>
+            <div class="col-lg-10">
+              <input type="text" class="form-control" name="name" value="John Doe <?=$id?>" required>
+            </div>
           </div>
-        </div>
 
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Shipper:</label>
-          <div class="col-lg-10">
-            <select name="shipper_id" class="form-control">
-                  <option value="<?=encode($value[0]['ShipperId'])?>"><?=$value[0]['ShipperName']?></option>
-              <?php foreach($couriers as $courier) { ?>
-                  <option value="<?=encode($courier['accounts_id'])?>"><?=$courier['name']?></option>
-              <?php } ?>
-            </select>
+          <div class="form-group row">
+            <label class="col-form-label col-lg-2">Email Address:</label>
+            <div class="col-lg-10">
+              <input type="email" class="form-control" name="email" value="JohnDoe<?=$id?>@gmail.com" required>
+            </div>
           </div>
-        </div>
 
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Consignee:</label>
-          <div class="col-lg-10">
-            <select name="consignee_id" class="form-control">
-              <option value="<?=encode($value[0]['ConsigneeId'])?>"><?=$value[0]['ConsigneeName']?></option>
-              <?php foreach($customers as $customer) { ?>
-                <option value="<?=encode($customer['accounts_id'])?>"><?=$customer['name']?></option>
-              <?php } ?>
-            </select>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Origin:</label>
-          <div class="col-lg-10">
-            <select name="origin_id" class="form-control">
-                  <option value="<?=encode($value[0]['OriginId'])?>"><?=$value[0]['Origin']?></option>
-              <?php foreach($provinces as $city) { ?>
-                <option value="<?=encode($city['province_id'])?>"><?=$city['provDesc']?></option>
-              <?php } ?>
-            </select>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Destination:</label>
-          <div class="col-lg-10">
-            <select name="destination_id" class="form-control">
-                  <option value="<?=encode($value[0]['DestinationId'])?>"><?=$value[0]['Destination']?></option>
+          <div class="form-group row">
+            <label class="col-form-label col-lg-2">Province:</label>
+            <div class="col-lg-10">
+              <select name="province_id" class="form-control">
+                    <!-- <option value="<?=encode($getUsers[0]['province_id'])?>" selected><?=$getUsers[0]['provDesc']?></option> -->
                 <?php foreach($provinces as $city) { ?>
                     <option value="<?=encode($city['province_id'])?>"><?=$city['provDesc']?></option>
                 <?php } ?>
               </select>
+            </div>
           </div>
-        </div>
 
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Address: (Optional)</label>
-          <div class="col-lg-10">
-            <input type="text" class="form-control" value="<?=$value[0]['Address']?>" name="address">
+          <div class="form-group row">
+            <label class="col-form-label col-lg-2">Address:</label>
+            <div class="col-lg-10">
+              <input type="text" class="form-control" name="address" value="John Doe <?=$id?>" required>
+            </div>
           </div>
-        </div>
-        
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Quantity:</label>
-          <div class="col-lg-10">
-            <input type="number" class="form-control" name="quantity" value="<?=$value[0]['Quantity']?>"  required>
-          </div>
-        </div>
 
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Actual Weight:</label>
-          <div class="col-lg-10">
-            <input type="text" class="form-control" name="description" value="<?=$value[0]['Description']?>"  required>
+          <div class="form-group row">
+            <label class="col-form-label col-lg-2">Status:</label>
+            <div class="col-lg-10">
+              <select name="status" class="form-control">
+                <!-- <option value="<?=$getUsers[0]['status']?>" selected><?=$getUsers[0]['status'] == 1 ? 'Not Active' : 'Active'?></option> -->
+                <option value="0">Active</option>
+                <option value="1">Not Active</option>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">CWT:</label>
-          <div class="col-lg-10">
-            <input type="text" class="form-control" name="cwt" value="<?=$value[0]['Cwt']?>"  required>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Pay Mode:</label>
-          <div class="col-lg-10">
-            <select name="pay_mode_id" class="form-control">
-            <option value="<?=encode($value[0]['PayModeId'])?>"><?=$value[0]['PayModeName']?></option>
-              <?php foreach($pay_modes as $pay_mode) { ?>
-                <option value="<?=encode($pay_mode['pay_mode_id'])?>"><?=$pay_mode['pay_mode_name']?></option>
-              <?php } ?>
-            </select>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Service Mode:</label>
-          <div class="col-lg-10">
-            <select name="service_mode_id" class="form-control">
-                  <option value="<?=encode($value[0]['ServiceModeId'])?>"><?=$value[0]['ServiceModeName']?></option>
-              <?php foreach($service_modes as $service_mode) { ?>
-                  <option value="<?=encode($service_mode['service_mode_id'])?>"><?=$service_mode['service_mode_name']?></option>
-              <?php } ?>
-            </select>
-          </div>
-        </div>
-
-        <div class="form-group row">
-          <label class="col-form-label col-lg-2">Amount:</label>
-          <div class="col-lg-10">
-            <input type="text" class="form-control" name="amount" value="<?=$value[0]['Amount']?>" required>
-          </div>
-        </div>
-
-        <div class="col-lg-10 ml-lg-auto text-right">
-            <button type="submit" class="btn btn-primary ml-3">Save Changes <i class="icon-paperplane ml-2"></i></button>
+          <div class="col-lg-10 ml-lg-auto text-right">
+          <button type="submit" class="btn btn-primary ml-3">Save Changes <i class="icon-paperplane ml-2"></i></button>
           </div>
         </div>
       </form>
